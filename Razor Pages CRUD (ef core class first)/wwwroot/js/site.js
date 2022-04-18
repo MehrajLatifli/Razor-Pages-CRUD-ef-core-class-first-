ed54e37c49e336bb9bb6b375ce4a0a766adada43
MehrajLatifli <meracletifli@gmail.com>
2022-04-18 12:54:06 +0400

    GOD bless World

--- Razor Pages CRUD (ef core class first)/wwwroot/js/site.js
+++ Razor Pages CRUD (ef core class first)/wwwroot/js/site.js
@@ -1,4 +1,28 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿$(document).ready(function () {
 
-// Write your JavaScript code.
+    $('th').click(function () {
+
+        var table = $(this).parents('table').eq(0)
+        var rows = table.find('tr:gt(0)').toArray().sort(comparer($(this).index()))
+        this.asc = !this.asc
+
+        if (!this.asc) {
+            rows = rows.reverse()
+        }
+        for (var i = 0; i < rows.length; i++) {
+            table.append(rows[i])
+        }
+    })
+
+    function comparer(index) {
+
+        return function (a, b) {
+            var valA = getCellValue(a, index), valB = getCellValue(b, index)
+            return $.isNumeric(valA) && $.isNumeric(valB) ? valA - valB : valA.toString().localeCompare(valB)
+        }
+    }
+
+    function getCellValue(row, index) {
+        return $(row).children('td').eq(index).text()
+    }
+});
